7d6104bc4f0c6e814ce0b95c97d3d45bd055c897
bobo <348828809@qq.com>
2019-12-23 08:10:34 +0800

    根据坐标计算距离

--- src/main/java/com/bobo/common/utils/DistanceUtil.java
+++ src/main/java/com/bobo/common/utils/DistanceUtil.java
@@ -12,29 +12,29 @@ public class DistanceUtil {
 	    }
 	    /**
 	     * 通过AB点经纬度获取距离
-	     * @param pointA A�?(经，�?)
-	     * @param pointB B�?(经，�?)
+	     * @param pointA A点(经，纬)
+	     * @param pointB B点(经，纬)
 	     * @return 距离(单位：米)
 	     */
 	    public static double calculate(Point2D pointA, Point2D pointB) {
 	        // 经纬度（角度）转弧度。弧度用作参数，以调用Math.cos和Math.sin
-	        double radiansAX = Math.toRadians(pointA.getX()); // A经弧�?
-	        double radiansAY = Math.toRadians(pointA.getY()); // A纬弧�?
-	        double radiansBX = Math.toRadians(pointB.getX()); // B经弧�?
-	        double radiansBY = Math.toRadians(pointB.getY()); // B纬弧�?
+	        double radiansAX = Math.toRadians(pointA.getX()); // A经弧度
+	        double radiansAY = Math.toRadians(pointA.getY()); // A纬弧度
+	        double radiansBX = Math.toRadians(pointB.getX()); // B经弧度
+	        double radiansBY = Math.toRadians(pointB.getY()); // B纬弧度
 	        double cos = Math.cos(radiansAY) * Math.cos(radiansBY) * Math.cos(radiansAX - radiansBX)
 	                + Math.sin(radiansAY) * Math.sin(radiansBY);
-	        double acos = Math.acos(cos); // 反余弦�??
-	        return EARTH_RADIUS * acos; // �?终结�?
+	        double acos = Math.acos(cos); // 反余弦值
+	        return EARTH_RADIUS * acos; // 最终结果
 	    }
 	    
 	  
 	    public static void main(String[] args) {
-	        // 北京 东单地铁�?
+	        // 北京 东单地铁站
 	       // Point2D pointDD = new Point2D.Double(116.425249, 39.914504);
-	        // 北京 西单地铁�?
+	        // 北京 西单地铁站
 	       // Point2D pointXD = new Point2D.Double(116.382001, 39.913329);
 	    	double d = getDistance(116.425249, 39.914504,116.382001, 39.913329);
-	        System.out.println("两点的距�?:"+d+"�?");
+	        System.out.println("两点的距离:"+d+"米");
 	    }
 }
